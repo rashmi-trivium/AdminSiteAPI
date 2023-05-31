8b66c82bd4d3f294d9513be9f8c1a72de226d830
ESOLUTIONS\Rashmi.Singh <rashmi.singh@trivium-esolutions.com>
2023-05-31 16:11:55 +0530

    Add project files.

---
+++ AdminSiteAPI/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AdminSiteAPI.Models;
+using System.Data.SqlClient;
+
+namespace AdminSiteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        public List<User> Index()
+        {
+            List<User> users = new List<User>();
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                users.Add(new User {
+                    Id = dr["id"].ToString(),
+                    CompanyID = dr["CompanyID"].ToString(),
+                    CompanyName = dr["CompanyName"].ToString(),
+                    Username = dr["Username"].ToString(),
+                    UserType = dr["UserType"].ToString(),
+                });
+            }
+            return users;
+        }
+    }
+}
---
+++ AdminSiteAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminSiteAPI.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string CompanyID { get; set; }
+        public string CompanyName { get; set;}
+        public string UserType { get; set; }
+    }
+}
