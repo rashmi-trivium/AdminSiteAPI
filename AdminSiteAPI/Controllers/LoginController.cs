d7beec1c4b8a118114989e5797dc0ba20597cff4
ESOLUTIONS\Rashmi.Singh <rashmi.singh@trivium-esolutions.com>
2023-06-01 17:05:53 +0530

    add login controller

---
+++ AdminSiteAPI/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using AdminSiteAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace AdminSiteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        [Route("api/login")]
+        public int Login([FromBody] LoginData data)
+        {
+            int response = 0;
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users where UserName=@uname AND password=@pwd", con);
+            cmd.Parameters.AddWithValue("@uname", data.UserName);
+            cmd.Parameters.AddWithValue("@pwd", data.Password);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                response = 1;
+            }
+            return response;
+        }
+    }
+}
---
+++ AdminSiteAPI/Models/LoginData.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminSiteAPI.Models
+{
+    public class LoginData
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
