e851f25a3f65cbfb60e67690132aa85596f5dc0b
ESOLUTIONS\Rashmi.Singh <rashmi.singh@trivium-esolutions.com>
2023-06-01 14:05:49 +0530

    apis for client actions

--- AdminSiteAPI/Controllers/AdminController.cs
+++ AdminSiteAPI/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using AdminSiteAPI.Models;
 using System.Data.SqlClient;
 using System.ComponentModel.Design;
+using System.Reflection;
 
 namespace AdminSiteAPI.Controllers
 {
@@ -77,6 +78,82 @@ namespace AdminSiteAPI.Controllers
             SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
             con.Open();
             SqlCommand cmd = new SqlCommand("delete from Users where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            int result = cmd.ExecuteNonQuery();
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("api/getclientlist")]
+        public List<Client> GetClientList()
+        {
+            List<Client> clients = new List<Client>();
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Clients", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                clients.Add(new Client
+                {
+                    ClientId = dr["ClientId"].ToString(),
+                    ClientName = dr["ClientName"].ToString(),
+                    Gender = dr["Gender"].ToString(),
+                    AnnualSalary = dr["AnnualSalary"].ToString(),
+                    DateOfBirth = dr["DateOfBirth"].ToString(),
+                });
+            }
+            return clients;
+        }
+
+        [HttpGet]
+        [Route("api/getclientlist/{id}")]
+        public Client GetCLientById(int id)
+        {
+            Client client = new Client();
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Clients where ClientId=@id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                client.ClientId = dr["ClientId"].ToString();
+                client.ClientName = dr["ClientName"].ToString();
+                client.Gender = dr["Gender"].ToString();
+                client.AnnualSalary = dr["AnnualSalary"].ToString();
+                client.DateOfBirth = dr["DateOfBirth"].ToString();
+            }
+            return client;
+        }
+
+        [HttpPost]
+        [Route("api/addclient")]
+        public int AddClient([FromBody] Client client)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Clients" +
+                "(ClientName, Gender, AnnualSalary, DateOfBirth)" +
+                " values(@ClientName, @Gender, @AnnualSalary, @DateOfBirth)", con);
+            cmd.Parameters.AddWithValue("@ClientName", client.ClientName);
+            cmd.Parameters.AddWithValue("@Gender", client.Gender);
+            cmd.Parameters.AddWithValue("@AnnualSalary", client.AnnualSalary);
+            cmd.Parameters.AddWithValue("@DateOfBirth", client.DateOfBirth);
+            int result = cmd.ExecuteNonQuery();
+
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("api/deleteclient/{id}")]
+        public int DeleteClient(int id)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from Clients where ClientId=@id", con);
             cmd.Parameters.AddWithValue("@id", id);
             int result = cmd.ExecuteNonQuery();
 
---
+++ AdminSiteAPI/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminSiteAPI.Models
+{
+    public class Client
+    {
+        public string? ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string Gender { get; set; }
+        public string AnnualSalary { get; set; }
+        public string DateOfBirth { get; set;}
+    }
+}
