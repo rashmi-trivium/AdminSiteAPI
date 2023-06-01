1de7382ef93a6e1d8ffea9fe1ae69985ecc17fc8
ESOLUTIONS\Rashmi.Singh <rashmi.singh@trivium-esolutions.com>
2023-06-01 12:13:47 +0530

    add functions for adding, deleting and fetching users

--- AdminSiteAPI/Controllers/AdminController.cs
+++ AdminSiteAPI/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AdminSiteAPI.Models;
 using System.Data.SqlClient;
+using System.ComponentModel.Design;
 
 namespace AdminSiteAPI.Controllers
 {
@@ -8,7 +9,9 @@ namespace AdminSiteAPI.Controllers
     [Route("[controller]")]
     public class AdminController : Controller
     {
-        public List<User> Index()
+        [HttpGet]
+        [Route("api/getuserlist")]
+        public List<User> GetUserList()
         {
             List<User> users = new List<User>();
             SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
@@ -26,6 +29,58 @@ namespace AdminSiteAPI.Controllers
                 });
             }
             return users;
+        }
+
+        [HttpGet]
+        [Route("api/getuserlist/{id}")]
+        public User GetUserById(int id)
+        {
+            User user = new User();
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                user.Id = dr["id"].ToString();
+                user.CompanyID = dr["CompanyID"].ToString();
+                user.CompanyName = dr["CompanyName"].ToString();
+                user.Username = dr["Username"].ToString();
+                user.UserType = dr["UserType"].ToString();
+            }
+            return user;
+        }
+
+        [HttpPost]
+        [Route("api/adduser")]
+        public int AddUser([FromBody] User user)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Users values(@id, @CompanyID, @CompanyName, @Username, @UserType)", con);
+            cmd.Parameters.AddWithValue("@id", user.Id);
+            cmd.Parameters.AddWithValue("@CompanyID", user.CompanyID);
+            cmd.Parameters.AddWithValue("@CompanyName", user.CompanyName);
+            cmd.Parameters.AddWithValue("@Username", user.Username);
+            cmd.Parameters.AddWithValue("@UserType", user.UserType);
+            int result = cmd.ExecuteNonQuery();
+
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("api/deleteuser/{id}")]
+        public int DeleteUser(int id)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=RASHMIS-LT\SQLEXPRESS;Initial Catalog=AdminSiteDB;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from Users where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            int result = cmd.ExecuteNonQuery();
+
+            return result;
         }
     }
 }
